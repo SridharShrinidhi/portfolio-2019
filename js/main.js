a2f94b38766e6d4c9a1202e34a3de2a31d299db7
Shrinidhi Sridhar <srid0002@algonquinlive.com>
2019-06-07 21:54:25 -0400

    Modify JS to change nav states
    Nav states were changed to highlight the section

--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
 $(".work-text").click(function(){
   $(".play").removeClass("show").addClass("hide");
+  $(".play-text>a>img").addClass("opacity");
   $(".about").removeClass("show").addClass("hide");
-  $(".work").removeClass("hide").addClass("show");
+  $(".about-text>a>img").addClass("opacity");
+  $(".work").removeClass("hide").addClass("show")
+  $(".work-text>a>img").removeClass("opacity");
 });
 
 $(".play-text").click(function(){
   $(".work").removeClass("show").addClass("hide");
+  $(".work-text>a>img").addClass("opacity");
   $(".about").removeClass("show").addClass("hide");
+  $(".about-text>a>img").addClass("opacity");
   $(".play").removeClass("hide").addClass("full-opacity");
+  $(".play-text>a>img").removeClass("opacity");
 });
 
 $(".about-text").click(function(){
   $(".work").removeClass("show").addClass("hide");
+  $(".work-text>a>img").addClass("opacity");
   $(".play").removeClass("show").addClass("hide");
+  $(".play-text>a>img").addClass("opacity");
   $(".about").removeClass("hide").addClass("show");
+  $(".about-text>a>img").removeClass("opacity");
+});
+
+$(document).ready(function() {
+  $(".play").removeClass("show").addClass("hide");
+  $(".play-text>a>img").addClass("opacity");
+  $(".about").removeClass("show").addClass("hide");
+  $(".about-text>a>img").addClass("opacity");
+  $(".work").removeClass("hide").addClass("show")
+  $(".work-text>a>img").removeClass("opacity");
 });
